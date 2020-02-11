eee81f5f65f7c253c7d0cbb8935f30d88124f1eb
Robby Marchesiello <robbymarchesiello@gmail.com>
2020-02-11 10:24:37 -0800

    set up sdai_player class

--- BattleShip/src/sdai_player.py
+++ BattleShip/src/sdai_player.py
@@ -1,29 +1,16 @@
-from typing import Dict, List
-import copy
+from typing import Dict, List, Tuple
+import math, random, copy, abc
 from . import game_config, board, ship, orientation, ship_placement, move
 from .player import Player
 from .firing_location_error import FiringLocationError
-import abc
+from .aiplayer import AIPlayer
 
-class SearchDestroyAIPlayer(Player):
+class HuntDestroyAIPlayer(AIPlayer):
     opponents: List["Player"]
     ships: Dict[str, ship.Ship]
 
-    def __init__(self, player_num: int, config: game_config.GameConfig, other_players: List["HumanPlayer"]) -> None:
+    def __init__(self, player_num: int, config: game_config.GameConfig, other_players: List[Player]) -> None:
         super().__init__(player_num, config, other_players)
-
-    def init_name(self, player_num: int, other_players: List["HumanPlayer"]) -> None:
-        pass
-
-    def get_orientation(self, ship_: ship.Ship) -> orientation.Orientation:
-        pass
-
-    def get_start_coords(self, ship_: ship.Ship):
-        pass
 
     def get_move(self) -> move.Move:
         pass
-
-    def fire_at(self, row: int, col: int) -> None:
-        pass
-
